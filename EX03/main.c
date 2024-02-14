dbe50c0cc4929b6766c94f655ee67a297ea9fee3
Fektaoui <159137013+Fektaoui@users.noreply.github.com>
2024-02-14 18:13:00 +0000

    Add files via upload

---
+++ EX03/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Entrer un nombre entier : \t");
+    scanf("%d",&n);
+    printf("Le double de ce nombre est : %d",2*n);
+    return 0;
+}
