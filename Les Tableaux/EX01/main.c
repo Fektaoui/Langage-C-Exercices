e0b3077f3050bf8613ebb582a667fc0ddb9120a3
Fektaoui <159137013+Fektaoui@users.noreply.github.com>
2025-02-19 18:39:56 +0000

    Add files via upload

---
+++ Les Tableaux/EX01/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int T[50],i,n;
+    long Som=0;
+    printf("Entrer la dimension du tableau :\t");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("T[%d]=",i);
+        scanf("%d",&T[i]);
+    }
+    for(i=0;i<n;i++){
+        printf("%7d",T[i]);
+    }
+    printf("\n");
+    for(i=0;i<n;i++){
+        Som+=T[i];
+    }
+    printf("La somme des %cl%cments du tableau est %ld",130,130,Som);
+    return 0;
+}
